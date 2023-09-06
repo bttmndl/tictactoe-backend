dea165227ff61cb343c3bdc6e5b8653ccd092591
bttmndl <bttmndl@gmail.com>
2023-09-07 00:32:09 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin:
+      "https://64f8c207692971112ecd39cd--fantastic-swan-79092d.netlify.app",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket)=>{
+    console.log(socket.id);
+    socket.on("board", (data)=>{
+        socket.broadcast.emit("board", data);
+    })
+    socket.on("currentPlayer", (data) => {
+      socket.broadcast.emit("currentPlayer", data);
+    });
+    socket.on("win", (data) => {
+      socket.broadcast.emit("win", data);
+    });
+    socket.on("draw", (data) => {
+      socket.broadcast.emit("draw", data);
+    });
+    socket.on("currID", (data) => {
+      socket.broadcast.emit("currID", data);
+    });
+})
+
+server.listen(4000, (req, res)=>{
+    console.log("server is running at port 4000")
+})
